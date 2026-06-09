e319ad3e4470bd48fe0a8b6c94818648493bef83
projectswithsujit <projectswithsujit@gmail.com>
2026-06-09 17:43:01 +0545

    Add files via upload

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,11 @@
+import "./footer.css"
+
+export default function Footer() {
+    return (
+        <footer class="app-footer">
+            <a class="credit-link" href="https://www.sujitmishra.com.np" target="_blank">
+                Created by Sujit Mishra.
+            </a>
+        </footer>
+    )
+}
